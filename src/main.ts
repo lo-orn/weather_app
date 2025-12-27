db5425a84111d08526786d218cb31ae32fff8bde
Lo <lovisaornestal@gmail.com>
2025-12-27 09:40:10 +0100

    pushing resporitory to git

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createHtml } from './models/utils/createHtml';
 import type { Weather } from './models/Weather';
 import './style.css'
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY
 
+console.log(import.meta.env.VITE_WEATHER_API_KEY)
 document.getElementById("searchForm")?.addEventListener("submit", async(e) => {
 e.preventDefault();
 const theInput = document.getElementById("searchText");
@@ -9,7 +11,7 @@ if(!theInput) return;
 
 const searchText = (theInput as HTMLInputElement).value
 
-const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=bf2b03041f104e94b2b60321252412&q=${encodeURIComponent(searchText)}&aqi=no`
+const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${encodeURIComponent(searchText)}&aqi=no`
 );
 const data: Weather = await response.json();
 console.log(data)
