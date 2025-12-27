f64d81d3df9c3817668b2a08307720b753b76a36
Lo <lovisaornestal@gmail.com>
2025-12-27 22:49:18 +0100

    added so that locationHeader is empty  when search is empty

--- src/models/utils/createHtml.ts
+++ src/models/utils/createHtml.ts
@@ -6,7 +6,6 @@ import { getWeatherQuote } from "./weatherQuote";
 
 export const createHtml = (weather: Weather) => {
     
-    
 const row = document.createElement("div")
 row.className = " gap-8 mt-4";
 const results = document.getElementById("results")
@@ -18,13 +17,11 @@ results.innerHTML = "";
 
 const locationHeader = document.getElementById("locationHeader") as HTMLHeadingElement | null
 if(!locationHeader) return; 
+locationHeader.innerHTML ="";
 locationHeader.textContent = weather.location.name
 results.parentElement?.insertBefore(locationHeader, results);
-
-
-
-
 locationHeader.textContent = weather.location.name;
+
 
 quoteEl.textContent = getWeatherQuote(
     weather.forecast.forecastday[0].day.condition.text,
